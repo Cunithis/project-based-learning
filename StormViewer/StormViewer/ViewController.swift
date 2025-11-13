c3cf48af324e60b8b379cd25621b34300908911b
Domantas Jocas <domantas0216@gmail.com>
2025-11-13 18:47:03 +0200

    Fixed app being created in SwiftUI instead of UIKit

--- StormViewer/StormViewer/ViewController.swift
+++ StormViewer/StormViewer/ViewController.swift
@@ -1,16 +1,32 @@
+//
+//  ViewController.swift
+//  StormViewer
+//
+//  Created by Domantas Jocas on 13/11/2025.
+//
+
 import UIKit
 
-class viewController: UIViewController {
+class ViewController: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        var pictures = [String]()
+        
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
+        
         let items = try! fm.contentsOfDirectory(atPath: path)
         
-        for item in items {
-            if item.hasPrefix("nssl"){
-                
+        for item in items{
+            if(item.hasPrefix("nssl")){
+                pictures.append(item)
             }
         }
+        print(pictures)
+        // Do any additional setup after loading the view.
     }
+
+
 }
+
