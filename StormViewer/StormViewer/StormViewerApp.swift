7273f1c546e02c2bd3d5e540168fe5e4f8148a1c
Domantas Jocas <domantas0216@gmail.com>
2025-11-13 18:25:41 +0200

    Initial commit: added projects

---
+++ StormViewer/StormViewer/StormViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StormViewerApp.swift
+//  StormViewer
+//
+//  Created by Domantas Jocas on 13/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct StormViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StormViewer/StormViewer/ViewController.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+class viewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl"){
+                
+            }
+        }
+    }
+}
